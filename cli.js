825a1f2d034c4e919a83d5f17c04d69f78cb47eb
wzahrt <wzahrt@unc.edu>
2023-04-17 12:44:41 -0400

    -h help command implemented

--- cli.js
+++ cli.js
@@ -6,3 +6,17 @@ import fetch from 'node-fetch';
 // process args
 var args = minimist(process.argv.slice(2));
 
+// logging info for help call
+if (args.h) {
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit`);
+	process.exit(0);
+}
+
+// Extracts the timezone
+const timezone = moment.tz.guess();
