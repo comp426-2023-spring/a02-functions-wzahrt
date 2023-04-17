2993d4d17de0c9478a93068c087566923e6dbe16
wzahrt <wzahrt@unc.edu>
2023-04-17 12:37:46 -0400

    Error in importing fixed

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+import minimist from 'minimist';
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+
+// process args
+var args = minimist(process.argv.slice(2));
+
